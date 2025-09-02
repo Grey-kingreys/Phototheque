17e562818584a36fc489c947cc145b2cb298b186
unknown <soulmamoudou0@gmail.com>
2025-09-02 16:10:25 +0000

    Commit sur le pc de l'entreprise

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const albumRoute = require('./routes/album.routes')
+const { title } = require('process');
+
+
+const app = express();
+const port = 3000;
+
+
+mongoose.connect('mongodb://localhost/phototheque');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('album', {title: 'Photothèque'})
+})
+
+app.use('/', albumRoute)
+
+app.use((req, res) => {
+    res.status(404);
+    res.send('Page non trouvé')
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
